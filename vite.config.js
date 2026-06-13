92d4ea03ad02c13991f5687f1d418de30c25681a
Danny Seo <sks8410@naver.com>
2026-06-13 16:10:40 +0900

    Updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/interactive_card_details_form/', // GitHub Pages 배포를 위한 서브디렉토리 경로 (Base URL) 설정
+  base: '/Interactive_card_details_form/', // GitHub Pages 배포를 위한 서브디렉토리 경로 (Base URL) 설정
   plugins: [
     react(),
     tailwindcss(), // Tailwind CSS (테일윈드 CSS) v4.0 통합 플러그인
